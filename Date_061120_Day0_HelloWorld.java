01a82469bf3ad20cf8f0483c02999cee571e0aca
13-akashnigam <13.akashnigam@gmail.com>
2020-06-11 01:43:09 +0530

    Solution for Day 0 Challenge

---
+++ Date_061120_Day0_HelloWorld.java
@@ -0,0 +1,16 @@
+package hackerRank_30DaysOfCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Date_061120_Day0_HelloWorld 
+{
+	public static void main(String[] args) throws IOException 
+	{		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String input = in.readLine();
+		System.out.println("Hello, World.");
+		System.out.println(input);
+	}
+}
